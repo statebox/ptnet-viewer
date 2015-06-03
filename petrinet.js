218fe37f73ff75025da059ac1f5e75791f65207c
Jelle Herold <jelle@defekt.nl>
2015-06-03 15:00:53 +0200

    specify net outside of petrinet.js

--- petrinet.js
+++ petrinet.js
@@ -1,42 +1,3 @@
-var states = [
-	{label:'a', y:60, x:20},
-	{label:'b0', y:30, x:100},
-	{label:'b1', y:90, x:100},
-	{label:'d0', y:30, x:180},
-	{label:'d1', y:90, x:180},
-	{label:'c', y:60, x:260}
-]
-
-var transitions = [
-
-	{label: 'x', y: 60, x: 60,
-		pre: {a: 1},
-		post: {
-			b0: 1,
-			b1: 1
-		}
-	},
-
-	{label: 'z0', y: 30, x: 140,
-		pre: {b0: 1},
-		post: {d0: 1}
-	},
-
-	{label: 'z1', y: 90, x: 140,
-		pre: {b1: 1},
-		post: {d1: 1}
-	},
-
-	{label: 'y', y: 60, x: 220,
-		pre: {
-			d0: 1,
-			d1: 1
-		},
-		post: {c: 1}}
-];
-
-var marking = {a: 1};
-
 function isEnabled(pre) {
 	return _(pre).all(function(multiplicity, place){
 		return (marking[place] || 0) >= multiplicity;
@@ -65,8 +26,6 @@ function updateMarking(remove, add){
 	
 	redraw();
 }
-
-(states.concat(transitions)).forEach(function(s){s.x *= 1.25;})
 
 function arcs(transitions) {
 
